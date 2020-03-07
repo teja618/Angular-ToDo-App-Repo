deb63c542b6f20530cc62526f76268bb1292ed2a
teja618 <teja618@gmail.com>
2020-03-07 17:20:34 +0530

    Simple To-Do List Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import {MatInputModule} from '@angular/material/input';
+import {TaskModel} from './TaskModel';
+import { from } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,18 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-To Do App';
-  inputTask=''
-  newItem:String=''
+  inputTask='';
+  tempTask:TaskModel;
+  taskList: TaskModel[] = [
+    {  "taskName": "Available Task 1" },
+    { "taskName": "Available Task 2" },
+    {"taskName": "Available Task 3"}
+];
 
-  items=[{type:String}]
+  delete=(index:number)=>{
+   this.taskList.splice(index,1);
+  }
 
   onAdd=()=>{
-  
-    this.items.push(
-      {type:this.newItem}
-      );
-
+   this.tempTask={taskName:this.inputTask};
+   this.taskList.push(this.tempTask);
   }
-  
-
 }
